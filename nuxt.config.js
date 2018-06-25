c0137daa3df079541c417a1a488f7127ccc8e6a2
Mike Holzbach <maholzbach@gmail.com>
2018-06-25 18:29:01 -0400

    Working on connecting with contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,19 +25,8 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend(config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/
-        });
-      }
-    }
+  build: {},
+  node: {
+    fs: "empty"
   }
 };
