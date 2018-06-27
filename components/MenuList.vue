25919d775110a5745fe3e9de5e29f0b2bc033775
Mike Holzbach <mikeholzbach@a-w3595htd6.carfax.cfx>
2018-06-27 09:45:40 -0400

    Scaffolded basic menuList component

---
+++ components/MenuList.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="menu-list">
+    <!-- <li v-for="item in menuItemsObj.coffeeItems" :key="item.itemName" class="menu-item">
+      {{item.itemName}} - ${{item.price}}
+    </li> -->
+    <li :key="key" class="menu-item class">{{name}} - ${{price}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String },
+    price: { type: String },
+    key: { type: String },
+    class: { type: String }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "~/assets/variables.scss";
+
+.menu-list {
+  list-style: none;
+  margin-bottom: 20px;
+}
+.menu-item {
+  font-size: 1.2em;
+  line-height: 30px;
+}
+.coffee {
+  grid-area: coffee;
+}
+.favorites {
+  grid-area: favorites;
+}
+.bsandp {
+  grid-area: bsandp;
+
+  @include md {
+    text-align: right;
+    margin-right: 20px;
+  }
+}
+.sandwiches {
+  grid-area: sandwiches;
+
+  @include md {
+    text-align: right;
+    margin-right: 20px;
+  }
+}
+</style>
+
--- pages/menu.vue
+++ pages/menu.vue
@@ -2,62 +2,65 @@
   <div class="menu">
     <h1 class="title">Menu</h1>
     <div class="menu-lists">
-    //! Componentize these and their logic
-    <ul class="menu-list coffee">
-      <li v-for="item in menuItemsObj.coffeeItems" :key="item.itemName" class="menu-item">
-        {{item.itemName}} - ${{item.price}}
-      </li>
-    </ul>
-    <ul class="menu-list sandwiches">
-      <li v-for="item in menuItemsObj.breakfastItems" :key="item.itemName" class="menu-item">
-        {{item.itemName}} - ${{item.price}}
-      </li>
-    </ul>
-    <ul class="menu-list bsandp">
-      <li v-for="item in menuItemsObj.breakfastSandwiches" :key="item.itemName" class="menu-item">
-        {{item.itemName}} - ${{item.price}}
-      </li>
-    </ul>
-    <ul class="menu-list favorites">
-      <li v-for="item in menuItemsObj.favorites" :key="item.itemName" class="menu-item">
-        {{item.itemName}} - ${{item.price}}
-      </li>
-    </ul>
+      <menu-list name="Iced Coffee" price="2.50" class="coffee" :key="name"></menu-list>
+      <ul class="menu-list coffee">
+        <li v-for="item in menuItemsObj.coffeeItems" :key="item.itemName" class="menu-item">
+          {{item.itemName}} - ${{item.price}}
+        </li>
+      </ul>
+      <ul class="menu-list sandwiches">
+        <li v-for="item in menuItemsObj.breakfastItems" :key="item.itemName" class="menu-item">
+          {{item.itemName}} - ${{item.price}}
+        </li>
+      </ul>
+      <ul class="menu-list bsandp">
+        <li v-for="item in menuItemsObj.breakfastSandwiches" :key="item.itemName" class="menu-item">
+          {{item.itemName}} - ${{item.price}}
+        </li>
+      </ul>
+      <ul class="menu-list favorites">
+        <li v-for="item in menuItemsObj.favorites" :key="item.itemName" class="menu-item">
+          {{item.itemName}} - ${{item.price}}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 import menuItems from "~/assets/contentful.json";
+import MenuList from "../components/MenuList.vue";
 
 const menuItemsObj = {
   coffeeItems: [],
   breakfastItems: [],
   breakfastSandwiches: [],
   favorites: []
-}
+};
 
 menuItems.forEach(menuItem => {
-  if(menuItem.sys.contentType.sys.id === "coffee") {
-    menuItemsObj.coffeeItems.push(menuItem.fields)
+  if (menuItem.sys.contentType.sys.id === "coffee") {
+    menuItemsObj.coffeeItems.push(menuItem.fields);
   }
-  if(menuItem.sys.contentType.sys.id === "breakfastItems") {
-    menuItemsObj.breakfastItems.push(menuItem.fields)
+  if (menuItem.sys.contentType.sys.id === "breakfastItems") {
+    menuItemsObj.breakfastItems.push(menuItem.fields);
   }
-  if(menuItem.sys.contentType.sys.id === "breakfastSandwiches") {
-    menuItemsObj.breakfastSandwiches.push(menuItem.fields)
+  if (menuItem.sys.contentType.sys.id === "breakfastSandwiches") {
+    menuItemsObj.breakfastSandwiches.push(menuItem.fields);
   }
-  if(menuItem.sys.contentType.sys.id === "mainStreetFavorites") {
-    menuItemsObj.favorites.push(menuItem.fields)
+  if (menuItem.sys.contentType.sys.id === "mainStreetFavorites") {
+    menuItemsObj.favorites.push(menuItem.fields);
   }
 });
 
 export default {
   data() {
-    console.log(menuItemsObj.coffeeItems);
     return {
       menuItemsObj
     };
+  },
+  components: {
+    MenuList
   }
 };
 </script>
@@ -93,35 +96,5 @@ export default {
 .list-title {
   color: $brand-brown;
   line-height: 40px;
-}
-.menu-list {
-  list-style: none;
-  margin-bottom: 20px;
-}
-.menu-item {
-  font-size: 1.2em;
-  line-height: 30px;
-}
-.coffee {
-  grid-area: coffee;
-}
-.favorites {
-  grid-area: favorites;
-}
-.bsandp {
-  grid-area: bsandp;
-
-  @include md {
-    text-align: right;
-    margin-right: 20px;
-  }
-}
-.sandwiches {
-  grid-area: sandwiches;
-
-  @include md {
-    text-align: right;
-    margin-right: 20px;
-  }
 }
 </style>
