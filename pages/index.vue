d8cfb7dab6010dd68d5f0e5a166b60a575749e2b
Mike Holzbach <maholzbach@gmail.com>
2018-07-14 12:39:31 -0400

    Added text icon and styling for online order link

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div>
-    <div class="content">
-      <div class="title-box">
-        <h1 class="title">Grounds Central Station</h1>
-        <h4 class="sub-title">9360 Main St. Manassas VA, 20110 </h4>
-      </div>
-      <div class="hours-box">
-        <h4 class="sub-title">
-          Mon-Thurs: 6am-9pm          
-        </h4>
-        <h4 class="sub-title">
-          Fri: 6am-10pm
-        </h4>
-        <h4 class="sub-title">
-          Sat: 7am-10pm
-        </h4>
-        <h4 class="sub-title">
-          Sun: 8am-8pm
-        </h4>
-      </div>
+  <div class="content">
+    <div class="title-box">
+      <h1 class="title">Grounds Central Station</h1>
+      <h4 class="sub-title">9360 Main St. Manassas VA, 20110 </h4>
+    </div>
+    <div class="hours-box">
+      <h4 class="sub-title">
+        Mon-Thurs: 6am-9pm          
+      </h4>
+      <h4 class="sub-title">
+        Fri: 6am-10pm
+      </h4>
+      <h4 class="sub-title">
+        Sat: 7am-10pm
+      </h4>
+      <h4 class="sub-title">
+        Sun: 8am-8pm
+      </h4>
+    </div>
+    <div class="online-order">
+      <i class="fas fa-utensils"></i>
+      Place your pickup orders here!
     </div>
   </div>
 </template>
@@ -36,19 +38,24 @@ export default {};
   font-family: Righteous, cursive;
   background-image: url("~/static/white-coffee-img.jpg");
   background-size: cover;
-  background-position: center;
+  background-position: 47% 50%;
   height: auto;
   min-height: 70vh;
   grid-gap: 20px;
   grid-template-areas:
     "title"
     "..."
-    "hours";
+    "hours"
+    "online-order";
 
   @include xs-to-sm {
     grid-template-rows: none;
     grid-template-columns: 1fr 1fr 1fr;
     grid-template-areas: "title ... hours";
+  }
+
+  @include md {
+    background-position: 39% 50%;
   }
 }
 .title-box {
@@ -67,12 +74,37 @@ export default {};
   grid-area: hours;
   align-self: center;
   text-align: center;
+  padding: 25px 0;
 }
 .sub-title {
   font-size: 1.2em;
 
   @include lg {
     font-size: 1.7em;
+  }
+}
+.online-order {
+  grid-area: hours;
+  align-self: end;
+  text-align: center;
+  font-size: 1.2em;
+  box-shadow: 0 0 0 rgba(150, 95, 65, 1);
+  animation: pulse 2s infinite;
+
+  @include md {
+    grid-area: title;
+    margin: 40px;
+  }
+}
+@keyframes pulse {
+  0% {
+    -webkit-box-shadow: 0 0 0 0 rgba(150, 95, 65, 1);
+  }
+  70% {
+    -webkit-box-shadow: 0 0 0 10px rgba(150, 95, 65, 0);
+  }
+  100% {
+    -webkit-box-shadow: 0 0 0 0 rgba(150, 95, 65, 0);
   }
 }
 </style>
